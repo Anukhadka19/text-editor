192eba8d02dbb57b1da8dbd99e99ddbb0e42a9f5
Anu Khadka <anuragi.khadka@gmail.com>
2023-03-06 19:36:43 -0500

    revisions to sw.js/webpack

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,19 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new CacheFirst({
+    // Name of the cache storage.
+    cacheName: 'page-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        title: 'Just Another Text Editor'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
